e912104155bf4ecf8edb4e14f92451e4fafc4199
S M Ahasanul Haque <smahasanulh@adobe.com>
2023-06-03 22:19:08 +0200

    added evaluation

--- seg/segment/predict.py
+++ seg/segment/predict.py
@@ -115,9 +115,10 @@ def run(
     # Run inference
     model.warmup(imgsz=(1 if pt else bs, 3, *imgsz))  # warmup
     seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
+    total_intersection = 0
+    total_union = 0
     for (path, im, im0s, vid_cap, s), (gt_path, gt_im, gt_im0s, gt_vid_cap, gt_s) in zip(dataset, ground_truth):
-        print(gt_path, gt_im.shape, gt_im0s.shape, gt_vid_cap, gt_s)
-
+        #print(gt_path, gt_im.shape, gt_im0s.shape, gt_vid_cap, gt_s)
         with dt[0]:
             im = torch.from_numpy(im).to(device)
             im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
@@ -158,8 +159,16 @@ def run(
                 masks = process_mask(proto[i], det[:, 6:], det[:, :4], im.shape[2:], upsample=True)  # HWC
                 is_person = det[:, 5] == 0
                 masks = torch.masked_select(masks, is_person.unsqueeze(1).unsqueeze(2)).view(-1, masks.shape[1], masks.shape[2])
-                print("tmp.shape:", masks.shape)
                 #print(type(masks), masks.shape, torch.unique(masks))
+                """
+                for i, mask in enumerate(masks):
+                    show_mask(f"/Users/ahasan/Downloads/personal/yolov7_object_detection/{i}.pcd", mask)
+                    #create_point_cloud_from_mask(mask)
+                    #output = mask.cpu().numpy()
+                    #cv2.imwrite(f'../../{i}.png', output)
+
+                exit()
+                """
                 # Rescale boxes from img_size to im0 size
                 #det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
                 # Print results
@@ -171,6 +180,8 @@ def run(
                 mcolors = [colors(int(cls), True) for cls in det[:, 5] if cls==0]
                 im_masks = plot_masks(im[i], masks, mcolors)  # image with masks shape(imh,imw,3)
                 annotator.im = scale_masks(im.shape[2:], im_masks, im0.shape)  # scale to original h, w
+                #print(type(annotator.im), annotator.im.shape, np.unique(annotator.im))
+                #exit(0)
 
                 # Mask plotting ----------------------------------------------------------------------------------------
 
@@ -193,9 +204,6 @@ def run(
                     if save_crop:
                         save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
                     """
-            import numpy as np
-            print(im0.shape, np.unique(im0))
-            exit(0)
             # Stream results
             im0 = annotator.result()
             if view_img:
@@ -205,6 +213,30 @@ def run(
                     cv2.resizeWindow(str(p), im0.shape[1], im0.shape[0])
                 cv2.imshow(str(p), im0)
                 cv2.waitKey(1)  # 1 millisecond
+            import numpy as np
+            imo_backup = im0.copy()
+            imo_backup[imo_backup != 0] = 1
+            print(
+              f'''
+              img: 
+              {(imo_backup[:,:,0]!=imo_backup[:,:,1]).sum()}
+              {(imo_backup[:,:,0]!=imo_backup[:,:,2]).sum()}
+              {(imo_backup[:,:,1]!=imo_backup[:,:,2]).sum()}
+              '''
+            )
+            print(
+              f'''
+              gt: 
+              {(gt_im0s[:,:,0]!=gt_im0s[:,:,1]).sum()}
+              {(gt_im0s[:,:,0]!=gt_im0s[:,:,2]).sum()}
+              {(gt_im0s[:,:,1]!=gt_im0s[:,:,2]).sum()}
+              '''
+            )
+            
+            intersection = np.sum(imo_backup * gt_im0s)
+            union = np.sum(imo_backup)  + np.sum(gt_im0s) - intersection
+            total_intersection += intersection
+            total_union += union
 
             # Save results (image with detections)
             if save_img:
@@ -227,7 +259,9 @@ def run(
 
         # Print time (inference-only)
         LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
+        break
 
+    print(f"mean IOU: {total_intersection/total_union}")
     # Print results
     t = tuple(x.t / seen * 1E3 for x in dt)  # speeds per image
     LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
@@ -243,7 +277,7 @@ def parse_opt():
     parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s-seg.pt', help='model path(s)')
     parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob, 0 for webcam')
     parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
-    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
+    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[1920], help='inference size h,w')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
     parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
